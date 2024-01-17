3952129a5e9ef1d2a1cbdfd3dd8a9f5bf58d5e7d
miracle <1650326658@qq.com>
2024-01-17 15:44:53 +0800

    添加项目文件。

---
+++ openGL/ZBufferStruct.h
@@ -0,0 +1,381 @@
+#pragma once
+#pragma once
+#include <Windows.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <gl/GL.h>
+#include <glm/glm.hpp>
+#include <string.h>
+#include <assimp/Importer.hpp>
+#include <assimp/scene.h>           // Assimp场景类的头文件
+#include <assimp/postprocess.h>     // Assimp后处理选项的头文件
+#include <vector>
+using namespace glm;
+using namespace std;
+#define MAX_BONE_INFLUENCE 4
+//定义颜色结构体
+struct Color {
+    GLubyte R, G, B;//R、G、B颜色
+};
+
+//定义顶点结构体
+struct zVertex {
+    GLfloat x, y, z;//x, y, z坐标
+};
+
+//定义多边形结构体
+struct zPolygon {
+    vector<zVertex> vertices;//顶点列表
+    Color color;//颜色
+};
+
+//定义分类多边形结点结构体
+struct ClassifyPolygonNode {
+    GLfloat a, b, c, d;//多边形所在平面方程系数
+    GLuint id;//多边形编号
+    GLint dy;//多边形跨越的扫描线数目
+    Color color;//多边形颜色
+    ClassifyPolygonNode* next;//下一个结点
+};
+
+//定义分类边结点结构体
+struct ClassifyEdgeNode {
+    GLdouble x;//边的上端点的x坐标
+    GLdouble dx;//相邻两条扫描线交点的x坐标差dx(-1/k)
+    GLint dy;//边跨越的扫描线数目
+    GLint id;//边所属的多边形编号
+    ClassifyEdgeNode* next;//下一个结点
+};
+
+//定义活化多边形结点结构体
+struct ActivePolygonNode {
+    GLfloat a, b, c, d;//多边形所在平面方程系数
+    GLuint id;//多边形编号
+    GLint dy;//多边形跨越的剩余扫描线数目
+    Color color;//多边形颜色
+    ActivePolygonNode* next;//下一个结点
+};
+
+//定义活化边结点结构体
+struct ActiveEdgeNode {
+    GLfloat xl;//左交点的x坐标
+    GLfloat dxl;//左交点两相邻扫描线交点的x坐标差
+    GLfloat dyl;//左交点两相邻扫描线交点的y坐标差
+    GLfloat xr;//右交点的x坐标
+    GLfloat dxr;//右交点两相邻扫描线交点的x坐标差
+    GLfloat dyr;//右交点两相邻扫描线交点的y坐标差
+    GLfloat zl;//左交点多边形所在平面深度值
+    GLfloat dzx;//扫描线右移一个像素时，多边形所在平面的深度增量
+    GLfloat dzy;//扫描线下移时，多边形所在平面的深度增量
+    GLuint id;//
+    ActiveEdgeNode* next;//下一个结点
+};
+
+/*class Mesh {
+public:
+    // mesh Data
+    vector<Vertex>       vertices;
+    vector<unsigned int> indices;
+    vector<Texture>      textures;
+    unsigned int VAO;
+
+    // constructor
+    Mesh(vector<Vertex> vertices, vector<unsigned int> indices, vector<Texture> textures)
+    {
+        this->vertices = vertices;
+        this->indices = indices;
+        this->textures = textures;
+
+        // now that we have all the required data, set the vertex buffers and its attribute pointers.
+        setupMesh();
+    }
+
+    // render the mesh
+    void Draw(Shader& shader)
+    {
+        // bind appropriate textures
+        unsigned int diffuseNr = 1;
+        unsigned int specularNr = 1;
+        unsigned int normalNr = 1;
+        unsigned int heightNr = 1;
+        for (unsigned int i = 0; i < textures.size(); i++)
+        {
+            glActiveTexture(GL_TEXTURE0 + i); // active proper texture unit before binding
+            // retrieve texture number (the N in diffuse_textureN)
+            string number;
+            string name = textures[i].type;
+            if (name == "texture_diffuse")
+                number = std::to_string(diffuseNr++);
+            else if (name == "texture_specular")
+                number = std::to_string(specularNr++); // transfer unsigned int to string
+            else if (name == "texture_normal")
+                number = std::to_string(normalNr++); // transfer unsigned int to string
+            else if (name == "texture_height")
+                number = std::to_string(heightNr++); // transfer unsigned int to string
+
+            // now set the sampler to the correct texture unit
+            glUniform1i(glGetUniformLocation(shader.ID, (name + number).c_str()), i);
+            // and finally bind the texture
+            glBindTexture(GL_TEXTURE_2D, textures[i].id);
+        }
+
+        // draw mesh
+        glBindVertexArray(VAO);
+        glDrawElements(GL_TRIANGLES, static_cast<unsigned int>(indices.size()), GL_UNSIGNED_INT, 0);
+        glBindVertexArray(0);
+
+        // always good practice to set everything back to defaults once configured.
+        glActiveTexture(GL_TEXTURE0);
+    }
+
+private:
+    // render data 
+    unsigned int VBO, EBO;
+
+    // initializes all the buffer objects/arrays
+    void setupMesh()
+    {
+        // create buffers/arrays
+        glGenVertexArrays(1, &VAO);
+        glGenBuffers(1, &VBO);
+        glGenBuffers(1, &EBO);
+
+        glBindVertexArray(VAO);
+        // load data into vertex buffers
+        glBindBuffer(GL_ARRAY_BUFFER, VBO);
+        // A great thing about structs is that their memory layout is sequential for all its items.
+        // The effect is that we can simply pass a pointer to the struct and it translates perfectly to a glm::vec3/2 array which
+        // again translates to 3/2 floats which translates to a byte array.
+        glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(Vertex), &vertices[0], GL_STATIC_DRAW);
+
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(unsigned int), &indices[0], GL_STATIC_DRAW);
+
+        // set the vertex attribute pointers
+        // vertex Positions
+        glEnableVertexAttribArray(0);
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)0);
+        // vertex normals
+        glEnableVertexAttribArray(1);
+        glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, Normal));
+        // vertex texture coords
+        glEnableVertexAttribArray(2);
+        glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, TexCoords));
+        // vertex tangent
+        glEnableVertexAttribArray(3);
+        glVertexAttribPointer(3, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, Tangent));
+        // vertex bitangent
+        glEnableVertexAttribArray(4);
+        glVertexAttribPointer(4, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, Bitangent));
+        // ids
+        glEnableVertexAttribArray(5);
+        glVertexAttribIPointer(5, 4, GL_INT, sizeof(Vertex), (void*)offsetof(Vertex, m_BoneIDs));
+
+        // weights
+        glEnableVertexAttribArray(6);
+        glVertexAttribPointer(6, 4, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, m_Weights));
+        glBindVertexArray(0);
+    }
+};
+
+class Shader
+{
+public:
+    unsigned int ID;
+    // constructor generates the shader on the fly
+    // ------------------------------------------------------------------------
+    Shader(const char* vertexPath, const char* fragmentPath, const char* geometryPath = nullptr)
+    {
+        // 1. retrieve the vertex/fragment source code from filePath
+        std::string vertexCode;
+        std::string fragmentCode;
+        std::string geometryCode;
+        std::ifstream vShaderFile;
+        std::ifstream fShaderFile;
+        std::ifstream gShaderFile;
+        // ensure ifstream objects can throw exceptions:
+        vShaderFile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+        fShaderFile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+        gShaderFile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+        try
+        {
+            // open files
+            vShaderFile.open(vertexPath);
+            fShaderFile.open(fragmentPath);
+            std::stringstream vShaderStream, fShaderStream;
+            // read file's buffer contents into streams
+            vShaderStream << vShaderFile.rdbuf();
+            fShaderStream << fShaderFile.rdbuf();
+            // close file handlers
+            vShaderFile.close();
+            fShaderFile.close();
+            // convert stream into string
+            vertexCode = vShaderStream.str();
+            fragmentCode = fShaderStream.str();
+            // if geometry shader path is present, also load a geometry shader
+            if (geometryPath != nullptr)
+            {
+                gShaderFile.open(geometryPath);
+                std::stringstream gShaderStream;
+                gShaderStream << gShaderFile.rdbuf();
+                gShaderFile.close();
+                geometryCode = gShaderStream.str();
+            }
+        }
+        catch (std::ifstream::failure& e)
+        {
+            std::cout << "ERROR::SHADER::FILE_NOT_SUCCESSFULLY_READ: " << e.what() << std::endl;
+        }
+        const char* vShaderCode = vertexCode.c_str();
+        const char* fShaderCode = fragmentCode.c_str();
+        // 2. compile shaders
+        unsigned int vertex, fragment;
+        // vertex shader
+        vertex = glCreateShader(GL_VERTEX_SHADER);
+        glShaderSource(vertex, 1, &vShaderCode, NULL);
+        glCompileShader(vertex);
+        checkCompileErrors(vertex, "VERTEX");
+        // fragment Shader
+        fragment = glCreateShader(GL_FRAGMENT_SHADER);
+        glShaderSource(fragment, 1, &fShaderCode, NULL);
+        glCompileShader(fragment);
+        checkCompileErrors(fragment, "FRAGMENT");
+        // if geometry shader is given, compile geometry shader
+        unsigned int geometry;
+        if (geometryPath != nullptr)
+        {
+            const char* gShaderCode = geometryCode.c_str();
+            geometry = glCreateShader(GL_GEOMETRY_SHADER);
+            glShaderSource(geometry, 1, &gShaderCode, NULL);
+            glCompileShader(geometry);
+            checkCompileErrors(geometry, "GEOMETRY");
+        }
+        // shader Program
+        ID = glCreateProgram();
+        glAttachShader(ID, vertex);
+        glAttachShader(ID, fragment);
+        if (geometryPath != nullptr)
+            glAttachShader(ID, geometry);
+        glLinkProgram(ID);
+        checkCompileErrors(ID, "PROGRAM");
+        // delete the shaders as they're linked into our program now and no longer necessary
+        glDeleteShader(vertex);
+        glDeleteShader(fragment);
+        if (geometryPath != nullptr)
+            glDeleteShader(geometry);
+
+    }
+    // activate the shader
+    // ------------------------------------------------------------------------
+    void use()
+    {
+        glUseProgram(ID);
+    }
+    // utility uniform functions
+    // ------------------------------------------------------------------------
+    void setBool(const std::string& name, bool value) const
+    {
+        glUniform1i(glGetUniformLocation(ID, name.c_str()), (int)value);
+    }
+    // ------------------------------------------------------------------------
+    void setInt(const std::string& name, int value) const
+    {
+        glUniform1i(glGetUniformLocation(ID, name.c_str()), value);
+    }
+    // ------------------------------------------------------------------------
+    void setFloat(const std::string& name, float value) const
+    {
+        glUniform1f(glGetUniformLocation(ID, name.c_str()), value);
+    }
+    // ------------------------------------------------------------------------
+    void setVec2(const std::string& name, const glm::vec2& value) const
+    {
+        glUniform2fv(glGetUniformLocation(ID, name.c_str()), 1, &value[0]);
+    }
+    void setVec2(const std::string& name, float x, float y) const
+    {
+        glUniform2f(glGetUniformLocation(ID, name.c_str()), x, y);
+    }
+    // ------------------------------------------------------------------------
+    void setVec3(const std::string& name, const glm::vec3& value) const
+    {
+        glUniform3fv(glGetUniformLocation(ID, name.c_str()), 1, &value[0]);
+    }
+    void setVec3(const std::string& name, float x, float y, float z) const
+    {
+        glUniform3f(glGetUniformLocation(ID, name.c_str()), x, y, z);
+    }
+    // ------------------------------------------------------------------------
+    void setVec4(const std::string& name, const glm::vec4& value) const
+    {
+        glUniform4fv(glGetUniformLocation(ID, name.c_str()), 1, &value[0]);
+    }
+    void setVec4(const std::string& name, float x, float y, float z, float w)
+    {
+        glUniform4f(glGetUniformLocation(ID, name.c_str()), x, y, z, w);
+    }
+    // ------------------------------------------------------------------------
+    void setMat2(const std::string& name, const glm::mat2& mat) const
+    {
+        glUniformMatrix2fv(glGetUniformLocation(ID, name.c_str()), 1, GL_FALSE, &mat[0][0]);
+    }
+    // ------------------------------------------------------------------------
+    void setMat3(const std::string& name, const glm::mat3& mat) const
+    {
+        glUniformMatrix3fv(glGetUniformLocation(ID, name.c_str()), 1, GL_FALSE, &mat[0][0]);
+    }
+    // ------------------------------------------------------------------------
+    void setMat4(const std::string& name, const glm::mat4& mat) const
+    {
+        glUniformMatrix4fv(glGetUniformLocation(ID, name.c_str()), 1, GL_FALSE, &mat[0][0]);
+    }
+
+private:
+    // utility function for checking shader compilation/linking errors.
+    // ------------------------------------------------------------------------
+    void checkCompileErrors(GLuint shader, std::string type)
+    {
+        GLint success;
+        GLchar infoLog[1024];
+        if (type != "PROGRAM")
+        {
+            glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+            if (!success)
+            {
+                glGetShaderInfoLog(shader, 1024, NULL, infoLog);
+                std::cout << "ERROR::SHADER_COMPILATION_ERROR of type: " << type << "\n" << infoLog << "\n -- --------------------------------------------------- -- " << std::endl;
+            }
+        }
+        else
+        {
+            glGetProgramiv(shader, GL_LINK_STATUS, &success);
+            if (!success)
+            {
+                glGetProgramInfoLog(shader, 1024, NULL, infoLog);
+                std::cout << "ERROR::PROGRAM_LINKING_ERROR of type: " << type << "\n" << infoLog << "\n -- --------------------------------------------------- -- " << std::endl;
+            }
+        }
+    }
+};
+
+class Model
+{
+public:
+    Model(char* path)
+    {
+        loadModel(path);
+    }
+    void Draw(Shader shader);
+private:
+    vector<Mesh> meshes;
+    string directory;
+    void loadModel(string path);
+    void processNode(aiNode* node, const aiScene* scene);
+    Mesh processMesh(aiMesh* mesh, const aiScene* scene);
+    vector<Texture> loadMaterialTextures(aiMaterial* mat, aiTextureType type,
+        string typeName);
+};
+void Model::Draw(Shader shader) {
+    for (unsigned int i = 0; i < meshes.size(); i++)
+        meshes[i].Draw(shader);
+}*/
---
+++ openGL/main.cpp
@@ -0,0 +1,378 @@
+#include <iostream>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <assimp/Importer.hpp>
+#include <assimp/scene.h>
+#include <assimp/postprocess.h>
+#include "ZBufferStruct.h"
+using namespace std;
+#define printV3(x) cout<<"("<<x[0]<<","<<x[1]<<","<<x[2]<<")"
+#define printColor3(x) cout<<"("<<x.r<<","<<x.g<<","<<x.b<<")"
+#define printV2(x) cout<<"("<<x[0]<<","<<x[1]<<")"
+bool Import(const string& pFile) {
+	Assimp::Importer importer;
+
+	const aiScene* scene = importer.ReadFile(pFile,
+		aiProcess_CalcTangentSpace|
+		aiProcess_Triangulate |
+		aiProcess_JoinIdenticalVertices|
+		aiProcess_SortByPType);
+
+	if (!scene) {
+		printf(importer.GetErrorString());
+		return false;
+	}
+	/*int meshes = scene->mNumMeshes;
+	for (int i = 0; i < meshes; i++) {
+		aiMesh* mesh = scene->mMeshes[i];
+		printf(mesh->mName.C_Str());
+		printf("\n");
+		cout<<mesh->mNumVertices<<endl;
+		for (int i = 0; i < mesh->mNumVertices; i++) {
+			cout << i << ":";
+			printV3(mesh->mVertices[i]);
+			cout << endl;
+			//printV3(mesh->mNormals[i]);
+			printV2(mesh->mTextureCoords[0][i]);
+			cout << endl;
+			cout << mesh->mNumFaces<<endl;
+ 
+		}
+	}*/
+	return true;
+}
+const int width = 800; //帧缓冲器的宽度
+const int height = 600; //帧缓冲器的高度
+const GLfloat minZ = -1000.0f; //最小深度值
+Color frameBuffer[width][height]; //帧缓冲器
+GLfloat zBuffer[width]; //z缓冲器
+vector<zPolygon> polygons; //多边形列表
+vector<ClassifyPolygonNode*> classifyPolygonTable; //分类多边形表
+vector<ClassifyEdgeNode*> classifyEdgeTable; //分类边表
+ActivePolygonNode* activePolygonHead; //活化多边形表头结点
+ActiveEdgeNode* activeEdgeHead; //活化边表头结点
+
+//计算多边形所在平面的方程系数
+void calculatePlane(zPolygon& polygon, GLfloat& a, GLfloat& b, GLfloat& c, GLfloat& d) {
+	//假设多边形是凸的，取前三个顶点构成一个平面
+	zVertex v1 = polygon.vertices[0];
+	zVertex v2 = polygon.vertices[1];
+	zVertex v3 = polygon.vertices[2];
+	//计算两个向量
+	GLfloat x1 = v2.x - v1.x;
+	GLfloat y1 = v2.y - v1.y;
+	GLfloat z1 = v2.z - v1.z;
+	GLfloat x2 = v3.x - v1.x;
+	GLfloat y2 = v3.y - v1.y;
+	GLfloat z2 = v3.z - v1.z;
+	//计算向量叉积
+	a = y1 * z2 - z1 * y2;
+	b = z1 * x2 - x1 * z2;
+	c = x1 * y2 - y1 * x2;
+	//计算常数项
+	d = -(a * v1.x + b * v1.y + c * v1.z);
+}
+//计算多边形的最高点和最低点的y坐标
+void calculateYRange(zPolygon& polygon, GLint& yMax, GLint& yMin) {
+	yMax = -1;
+	yMin = height;
+	for (zVertex& v : polygon.vertices) {
+		//将顶点的y坐标转换为整数
+		GLint y = round(v.y);
+		//更新y的最大值和最小值
+		if (y > yMax) yMax = y;
+		if (y < yMin) yMin = y;
+	}
+}
+
+//计算多边形跨越的扫描线数目
+GLint calculateDY(zPolygon& polygon) {
+	GLint yMax, yMin;
+	calculateYRange(polygon, yMax, yMin);
+	return yMax - yMin + 1;
+}
+//构建分类多边形边表的函数
+void buildClassifyPolygonEdgeTable() {
+	//初始化分类多边形表和分类边表
+	for (int i = 0; i < height; i++) {
+		classifyPolygonTable.push_back(NULL);
+		classifyEdgeTable.push_back(NULL);
+	}
+	//遍历所有多边形
+	for (int i = 0; i < polygons.size(); i++) {
+		zPolygon& polygon = polygons[i];
+		//计算多边形的平面方程系数
+		GLfloat a, b, c, d;
+		calculatePlane(polygon, a, b, c, d);
+		//计算多边形的最高点和最低点的y坐标
+		GLint yMax, yMin;
+		calculateYRange(polygon, yMax, yMin);
+		//创建一个分类多边形结点
+		ClassifyPolygonNode* cpn = new ClassifyPolygonNode();
+		cpn->a = a;
+		cpn->b = b;
+		cpn->c = c;
+		cpn->d = d;
+		cpn->id = i;
+		cpn->dy = calculateDY(polygon);
+		cpn->color = polygon.color;
+		cpn->next = NULL;
+		//将分类多边形结点插入到分类多边形表的对应位置
+		ClassifyPolygonNode* cpnHead = classifyPolygonTable[yMin];
+		if (cpnHead == NULL) {
+			classifyPolygonTable[yMin] = cpn;
+		}
+		else {
+			while (cpnHead->next != NULL) {
+				cpnHead = cpnHead->next;
+			}
+			cpnHead->next = cpn;
+		}
+		//遍历多边形的所有边
+		for (int j = 0; j < polygon.vertices.size(); j++) {
+			//获取边的两个端点
+			zVertex v1 = polygon.vertices[j];
+			zVertex v2 = polygon.vertices[(j + 1) % polygon.vertices.size()];
+			//将端点的y坐标转换为整数
+			GLint y1 = round(v1.y);
+			GLint y2 = round(v2.y);
+			//如果端点的y坐标相同，说明是水平边，跳过
+			if (y1 == y2) continue;
+			//如果端点的y坐标不同，说明是非水平边，创建一个分类边结点
+			ClassifyEdgeNode* cen = new ClassifyEdgeNode();
+			//计算边的斜率
+			GLdouble k = (v2.x - v1.x) / (v2.y - v1.y);
+			//根据端点的y坐标大小，确定边的上端点和下端点
+			if (y1 < y2) {
+				cen->x = v1.x;
+				cen->dx = -1.0 / k;
+				cen->dy = y2 - y1;
+			}
+			else {
+				cen->x = v2.x;
+				cen->dx = 1.0 / k;
+				cen->dy = y1 - y2;
+			}
+			//设置边所属的多边形编号
+			cen->id = i;
+			cen->next = NULL;
+			//将分类边结点插入到分类边表的对应位置
+			ClassifyEdgeNode* cenHead = classifyEdgeTable[std::min(y1, y2)];
+			if (cenHead == NULL) {
+				classifyEdgeTable[std::min(y1, y2)] = cen;
+			}
+			else {
+				while (cenHead->next != NULL) {
+					cenHead = cenHead->next;
+				}
+				cenHead->next = cen;
+			}
+		}
+	}
+}
+
+//将分类多边形表和分类边表中的结点移动到活化多边形表和活化边表中的函数
+void moveClassifyNodesToActiveNodes(GLint y) {
+	//从分类多边形表中取出当前扫描线对应的多边形结点
+	ClassifyPolygonNode* cpn = classifyPolygonTable[y];
+	while (cpn != NULL) {
+		//创建一个活化多边形结点
+		ActivePolygonNode* apn = new ActivePolygonNode();
+		apn->a = cpn->a;
+		apn->b = cpn->b;
+		apn->c = cpn->c;
+		apn->d = cpn->d;
+		apn->id = cpn->id;
+		apn->dy = cpn->dy;
+		apn->color = cpn->color;
+		apn->next = NULL;
+		//将活化多边形结点插入到活化多边形表的头部
+		apn->next = activePolygonHead;
+		activePolygonHead = apn;
+		//释放分类多边形结点的内存
+		ClassifyPolygonNode* temp = cpn;
+		cpn = cpn->next;
+		delete temp;
+	}
+	//从分类边表中取出当前扫描线对应的边结点
+	ClassifyEdgeNode* cen = classifyEdgeTable[y];
+	while (cen != NULL) {
+		//创建一个活化边结点
+		ActiveEdgeNode* aen = new ActiveEdgeNode();
+		aen->xl = cen->x;
+		aen->dxl = cen->dx;
+		aen->dyl = cen->dy;
+		aen->xr = cen->x;
+		aen->dxr = cen->dx;
+		aen->dyr = cen->dy;
+		aen->zl = 0.0f;
+		aen->dzx = 0.0f;
+		aen->dzy = 0.0f;
+		aen->id = cen->id;
+		aen->next = NULL;
+		//将活化边结点插入到活化边表的头部
+		aen->next = activeEdgeHead;
+		activeEdgeHead = aen;
+		//释放分类边结点的内存
+		ClassifyEdgeNode* temp = cen;
+		cen = cen->next;
+		delete temp;
+	}
+}
+
+//对活化边表中的结点进行排序的函数
+void sortActiveEdgeNodes() {
+	//使用冒泡排序算法
+	ActiveEdgeNode* p = activeEdgeHead;
+	while (p != NULL) {
+		ActiveEdgeNode* q = p->next;
+		while (q != NULL) {
+			//如果左交点的x坐标较大，交换两个结点的数据
+			if (p->xl > q->xl) {
+				swap(p->xl, q->xl);
+				swap(p->dxl, q->dxl);
+				swap(p->dyl, q->dyl);
+				swap(p->xr, q->xr);
+				swap(p->dxr, q->dxr);
+				swap(p->dyr, q->dyr);
+				swap(p->zl, q->zl);
+				swap(p->dzx, q->dzx);
+				swap(p->dzy, q->dzy);
+				swap(p->id, q->id);
+			}
+			q = q->next;
+		}
+		p = p->next;
+	}
+}
+
+//计算活化边表中的交点对的深度值和深度增量的函数
+void calculateDepthAndIncrement() {
+	GLint y = round(activeEdgeHead->xl);
+	//遍历活化边表中的所有结点
+	ActiveEdgeNode* aen = activeEdgeHead;
+	while (aen != NULL) {
+		//根据交点对所在的多边形编号，从活化多边形表中找到对应的平面方程系数
+		ActivePolygonNode* apn = activePolygonHead;
+		while (apn != NULL) {
+			if (apn->id == aen->id) {
+				//计算左交点的深度值
+				aen->zl = -(apn->a * aen->xl + apn->b * y + apn->d) / apn->c;
+				//计算深度增量
+				aen->dzx = -apn->a / apn->c;
+				aen->dzy = -apn->b / apn->c;
+				break;
+			}
+			apn = apn->next;
+		}
+		aen = aen->next;
+	}
+}
+
+//遍历活化边表中的交点对，更新帧缓冲器和z缓冲器的函数
+void updateFrameBufferAndZBuffer(GLint y) {
+	//遍历活化边表中的所有结点
+	ActiveEdgeNode* aen = activeEdgeHead;
+	while (aen != NULL) {
+		//将左交点和右交点的x坐标转换为整数
+		GLint xl = round(aen->xl);
+		GLint xr = round(aen->xr);
+		//初始化当前深度值为左交点的深度值
+		GLfloat z = aen->zl;
+		//遍历左交点和右交点之间的所有像素
+		for (GLint x = xl; x <= xr; x++) {
+			//如果当前深度值小于z缓冲器中的深度值，说明该像素可见
+			if (z < zBuffer[x]) {
+				//更新z缓冲器中的深度值
+				zBuffer[x] = z;
+				//根据交点对所在的多边形编号，从活化多边形表中找到对应的颜色
+				ActivePolygonNode* apn = activePolygonHead;
+				while (apn != NULL) {
+					if (apn->id == aen->id) {
+						//更新帧缓冲器中的颜色
+						frameBuffer[x][y] = apn->color;
+						break;
+					}
+					apn = apn->next;
+				}
+			}
+			//更新当前深度值，使用增量方法
+			z += aen->dzx;
+		}
+		aen = aen->next;
+	}
+}
+
+//更新活化多边形表和活化边表中的结点的函数
+void updateActiveNodes(GLint y) {
+	//创建一个活化多边形表的临时头结点
+	ActivePolygonNode* apnTemp = new ActivePolygonNode();
+	apnTemp->next = activePolygonHead;
+	//创建一个活化边表的临时头结点
+	ActiveEdgeNode* aenTemp = new ActiveEdgeNode();
+	aenTemp->next = activeEdgeHead;
+	//遍历活化多边形表中的所有结点
+	ActivePolygonNode* apn = apnTemp;
+	while (apn->next != NULL) {
+		//减少多边形跨越的剩余扫描线数目
+		apn->next->dy--;
+		//如果多边形跨越的剩余扫描线数目为0，说明该多边形已经处理完，删除该结点
+		if (apn->next->dy == 0) {
+			ActivePolygonNode* temp = apn->next;
+			apn->next = apn->next->next;
+			delete temp;
+		}
+		else {
+			apn = apn->next;
+		}
+	}
+	//遍历活化边表中的所有结点
+	ActiveEdgeNode* aen = aenTemp;
+	while (aen->next != NULL) {
+		//减少边跨越的剩余扫描线数目
+		aen->next->dyl--;
+		aen->next->dyr--;
+		//如果边跨越的剩余扫描线数目为0，说明该边已经处理完，删除该结点
+		if (aen->next->dyl == 0 || aen->next->dyr == 0) {
+			ActiveEdgeNode* temp = aen->next;
+			aen->next = aen->next->next;
+			delete temp;
+		}
+		else {
+			//增加交点的x坐标，使用增量方法
+			aen->next->xl += aen->next->dxl;
+			aen->next->xr += aen->next->dxr;
+			aen = aen->next;
+		}
+	}
+	//更新活化多边形表和活化边表的头结点
+	activePolygonHead = apnTemp->next;
+	activeEdgeHead = aenTemp->next;
+	//释放临时头结点的内存
+	delete apnTemp;
+	delete aenTemp;
+}
+int main()
+{
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	GLFWwindow* window = glfwCreateWindow(800, 600, "openGL",NULL,NULL);
+	glfwMakeContextCurrent(window);
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)){
+		cout<<"Failed to initialize GLAD"<<endl;
+		glfwTerminate();
+	}
+	char* path = (char*)"C:\\Users\\16503\\Desktop\\Utah\\source\\ba31ad9775cb44ee861971b805a29f68\\teapot.obj";
+	//Model model(path);
+	//Import("C:\\Users\\16503\\Desktop\\Utah\\source\\ba31ad9775cb44ee861971b805a29f68\\teapot.obj");
+	while (!glfwWindowShouldClose(window)) {
+		glClearColor(0.2f,0.3f,0.3f,1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+		glfwPollEvents();
+		glfwSwapBuffers(window);
+	}
+	glfwTerminate();
+    return 0;
+}
